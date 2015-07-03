1c4898f6c5e8d76cbf97fe6a6d4fd5cd1af0ec2f
tenshiPure <user.ryo@gmail.com>
2015-07-03 21:08:12 +0900

    PersonDetailR get

--- Handler/PersonDetail.hs
+++ Handler/PersonDetail.hs
@@ -3,4 +3,8 @@ module Handler.PersonDetail where
 import Import
 
 getPersonDetailR :: PersonId -> Handler Html
-getPersonDetailR personId = error "Not yet implemented: getPersonDetailR"
+getPersonDetailR personId = do
+    person <- runDB $ get404 personId
+    defaultLayout $ do
+        setTitle $ toHtml $ personName person
+        $(widgetFile "person/detail")

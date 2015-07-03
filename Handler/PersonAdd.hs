389d14e45f0bfa9ca12ee333c308eb39047722c7
tenshiPure <user.ryo@gmail.com>
2015-07-03 21:19:59 +0900

    PersonAddR get post

---
+++ Handler/PersonAdd.hs
@@ -0,0 +1,25 @@
+module Handler.PersonAdd where
+
+import Import
+
+form :: Form Person
+form = renderDivs $ Person
+    <$> areq textField "名前" Nothing
+    <*> areq textField "楽器" Nothing
+
+getPersonAddR :: Handler Html
+getPersonAddR = do
+    (personWidget, enctype) <- generateFormPost form
+    defaultLayout $(widgetFile "person/add")
+
+postPersonAddR :: Handler Html
+postPersonAddR = do
+    ((res, personWidget), enctype) <- runFormPost form
+    case res of
+        FormSuccess person -> do
+            personId <- runDB $ insert person
+            setMessage $ toHtml $ (personName person) <> " created"
+            redirect $ PersonDetailR personId
+        _ -> defaultLayout $ do
+            setTitle "Invalid Input"
+            $(widgetFile "person/add")
--- Handler/PersonList.hs
+++ Handler/PersonList.hs
@@ -4,13 +4,8 @@ getPersonListR
 
 import Import
 
--- form :: Form Person
--- form = renderDivs $ Person
---     <$> areq textField "名前" Nothing
---     <*> areq textField "楽器" Nothing
-
 getPersonListR :: Handler Html
 getPersonListR = do
     persons <- runDB $ selectList [] [Desc PersonId]
 
-    defaultLayout $(widgetFile "persons")
+    defaultLayout $(widgetFile "person/list")

7c64695d7ae1bd423d2d0b1fad1045b57a7a8538
tenshiPure <user.ryo@gmail.com>
2015-07-05 10:50:43 +0900

    verb

---
+++ Handler/Sample.hs
@@ -0,0 +1,26 @@
+module Handler.Sample where
+
+
+import Import
+
+
+form :: Form Sample
+form = renderDivs $ Sample
+    <$> areq textField "名前" Nothing
+
+
+getSampleR :: Handler Html
+getSampleR = do
+    (widget, enctype) <- generateFormPost form
+
+    defaultLayout $(widgetFile "sample")
+
+
+postSampleR :: Handler Html
+postSampleR = do
+    verb <- lookupPostParam "verb"
+    case verb of
+        Just "create" -> defaultLayout [whamlet|<p>create!|]
+        Just "update" -> defaultLayout [whamlet|<p>update!|]
+        Just "delete" -> defaultLayout [whamlet|<p>delete!|]
+        _             -> defaultLayout [whamlet|<p>error!|]

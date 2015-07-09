a5d7ff34908d58ba962ab16a04406110631a9478
tenshiPure <user.ryo@gmail.com>
2015-07-09 22:31:20 +0900

    experience list -> update

--- Handler/Experience.hs
+++ Handler/Experience.hs
@@ -4,19 +4,26 @@ module Handler.Experience where
 import Import
 
 
-data Es = Es { ePersonId :: PersonId, eInstrumentId :: InstrumentId, eMaybeYears :: Maybe Int, eInstrumentName ::  Text }
+data Es = Es { ePersonId :: PersonId, eInstrumentId :: InstrumentId, eMaybeYears :: Maybe Int, eInstrumentName ::  Text, eInstrumentIcon :: Text }
 
-getExperienceListR :: PersonId -> Handler Html
-getExperienceListR personId = do
+
+getExperienceUpdateR :: PersonId -> Handler Html
+getExperienceUpdateR personId = do
     person <- runDB $ get404 personId
     instruments <- runDB $ selectList [] [Asc InstrumentId]
 
     es <- forM instruments $ \instrument -> do
         let instrumentId' = entityKey instrument
         let instrumentName' = instrumentName $ entityVal instrument
+        let instrumentIcon' = instrumentIcon $ entityVal instrument
         e <- runDB $ selectFirst [ExperiencePersonId ==. personId, ExperienceInstrumentId ==. instrumentId'] [Asc ExperienceId]
         let years' = experienceYears . entityVal <$> e
 
-        return $ Es personId instrumentId' years' instrumentName'
+        return $ Es personId instrumentId' years' instrumentName' instrumentIcon'
 
     defaultLayout $(widgetFile "experience/list")
+
+
+postExperienceUpdateR :: PersonId -> Handler Html
+postExperienceUpdateR personId = do
+    defaultLayout [whamlet| hello |]

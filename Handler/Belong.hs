925b9812bcc7173938823c9325e37f948c578132
tenshiPure <user.ryo@gmail.com>
2015-07-12 01:52:14 +0900

    invite pulldown has not invited person

--- Handler/Belong.hs
+++ Handler/Belong.hs
@@ -10,7 +10,10 @@ fBelong groupId = renderDivs $ Belong
     <*> areq hiddenField "" (Just groupId)
     where
         sections = do
-            persons <- runDB $ selectList [] [Asc PersonId]
+            belongs <- runDB $ selectList [BelongGroupId ==. groupId] [Asc BelongId]
+            let belongedPersonIds = map (belongPersonId . entityVal) belongs
+            persons <- runDB $ selectList [PersonId /<-. belongedPersonIds] [Asc PersonId]
+
             optionsPairs $ map (\p -> (personName $ entityVal p, entityKey p)) persons
 
 
@@ -27,6 +30,9 @@ postBelongCreateR groupId = do
     case res of
         FormSuccess belong -> do
             _ <- runDB $ insert belong
+            redirect $ GroupDetailR groupId
+        _ -> do
+            setMessage "nothing to do..."
             redirect $ GroupDetailR groupId
 
 

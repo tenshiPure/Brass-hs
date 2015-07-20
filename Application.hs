37473abe75ad21e31cd82a82c65a50f9771a0495
tenshiPure <user.ryo@gmail.com>
2015-07-20 21:30:38 +0900

    remove some files

--- Application.hs
+++ Application.hs
@@ -40,7 +40,6 @@ import Handler.Attendance
 import Handler.Link
 import Handler.Message
 import Handler.Home
-import Handler.Debug
 
 -- This line actually creates our YesodDispatch instance. It is the second half
 -- of the call to mkYesodData which occurs in Foundation.hs. Please see the

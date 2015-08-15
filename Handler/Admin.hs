5c3905d506a649e75996cf440ff7b68dc908b53c
tenshiPure <user.ryo@gmail.com>
2015-08-15 22:23:18 +0900

    rest sample

--- Handler/Admin.hs
+++ Handler/Admin.hs
@@ -149,9 +149,19 @@ getAdminInitR = do
     redirect $ AdminLinksR
 
 
-getAdminWorkspaceR :: Handler Html
-getAdminWorkspaceR = do
-    defaultLayout [whamlet| hello |]
+getAdminWorkspaceR :: Handler TypedContent
+getAdminWorkspaceR = selectRep $ do
+    provideRep $ return
+        [shamlet|
+            <p>Hello, my name is #{name} and I am #{age} years old.
+        |]
+    provideRep $ return $ object
+        [ "name" .= name
+        , "age" .= age
+        ]
+    where
+        name = "Michael" :: Text
+        age = 28 :: Int
 
 
 postAdminDebugLoginR :: Handler Html
